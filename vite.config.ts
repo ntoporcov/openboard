45f8b6f703dcfb6a4027b93bfe5dce8e8d569cfa
Nic Toporcov <ntoporcov@me.com>
2026-05-06 11:11:18 -0400

    Build Pages artifacts in docs

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/openboard/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react(), tailwindcss()],
 })
